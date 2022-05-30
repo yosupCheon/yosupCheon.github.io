4d124c4d9905bbefb65a618585c49cbd72d889a5
Yosup Cheon <yosupc@sfu.ca>
2022-05-29 23:57:40 -0700

    fixed error

--- src/main.js
+++ src/main.js
@@ -2,19 +2,20 @@ let userVal;
 let randVal;
 document.getElementById("button-submit").addEventListener("click", compare);
 
-function compare () {
-    userVal = document.getElementById("user-input").value;
-    randVal = getRandomNum();
-    document.getElementById("correct-result").innerHTML = randVal;
-    document.getElementById("result").innerHTML = matchDetermine(userVal, randVal);
-}
-
 const getRandomNum=()=>{
     return Math.floor(Math.random()*11);
 }
 
+function compare () {
+    userVal = document.getElementById("user-input").value;
+    randVal = getRandomNum();
+    document.getElementById("correct-result").innerHTML = randVal;
+    if (isNaN(parseInt(0 <= randVal || randVal <= 10))){userVal=12;}
+    document.getElementById("result").innerHTML = matchDetermine(userVal, randVal);
+}
+
 function matchDetermine (num, guess) {
-    if (parseInt(num)===guess){return 'Guessed Correctly!';}
+    if (parseInt(num)===(guess)){return 'Guessed Correctly!';}
     else if (isNaN(parseInt(num)) || parseInt(num) < 0 || 10 < parseInt(num) ){
         return 'please enter a valid integer';}
     else {return' Wrong Guess...';}
